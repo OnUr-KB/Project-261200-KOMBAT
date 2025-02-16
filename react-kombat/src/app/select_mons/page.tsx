dafcf50fdb34cde8a05b898c2c33fa5ccd7c1ee8
EileenIL <sadanun.en@gmail.com>
2025-02-16 18:06:48 +0700

    add_select_player_and_monster_

---
+++ react-kombat/src/app/select_mons/page.tsx
@@ -0,0 +1,130 @@
+"use client";
+import React from 'react'
+import { useRouter } from "next/navigation";
+import { Flex, Radio, RadioGroup ,Button} from '@mantine/core';
+export default function selectmons() {
+   const router = useRouter();
+  return (
+    <div>
+     {/* พื้นหลัง */}
+     <div
+        style={{
+          width: "100%",
+          height: "100vh",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          backgroundImage: "url(/image/Desktop_select_mons.jpg)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          zIndex: -1,
+        }}
+      />
+      <div
+  className="d-flex justify-content-center align-items-center gap-5"
+  style={{
+    position: "fixed",
+    top: "50%", // ตั้งให้อยู่ตรงกลางแนวตั้ง
+    left: "50%", // ตั้งให้อยู่ตรงกลางแนวนอน
+    transform: "translate(-50%, -50%)", // เลื่อนกลับครึ่งหนึ่งของขนาดตัวเองเพื่อให้กลางจุด
+    zIndex: 10, // ให้มันอยู่เหนือทุกอย่าง
+  }}
+>
+  <img
+    src="/image/pandy.png"
+    className="rounded img-fluid"
+    alt="mode1"
+    style={{
+      maxWidth: "200px",
+      marginBottom: "3vh",
+    }}
+    onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.9")}
+    onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
+  />
+  <img
+    src="/image/cheese_bear.png"
+    className="rounded img-fluid"
+    alt="mode2"
+    style={{
+      maxWidth: "200px",
+      marginBottom: "3vh",
+    }}
+    onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.9")}
+    onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
+  />
+  <img
+    src="/image/moonum.png"
+    className="rounded img-fluid"
+    alt="mode3"
+    style={{
+      maxWidth: "200px",
+      marginBottom: "3vh",
+    }}
+    onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.9")}
+    onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
+  />
+</div>
+<Flex
+      direction={{ base: 'column', sm: 'row' }}
+      gap={{ base: 'sm', sm: 'lg' }}
+      justify={{ sm: 'center' }}
+      style={{
+        position: "fixed",
+        bottom: "20%", // ปรับให้ลงมาต่ำกว่าขอบจอเล็กน้อย
+        left: "50%",
+        transform: "translateX(-50%)", // ทำให้ปุ่มอยู่กึ่งกลางแนวนอน
+        width: "100%", // ทำให้ปุ่มกระจายเท่ากัน
+        //margin:"20%"
+        
+      }}
+    >
+      <img
+          src="/image/selectmons.png"
+          alt="Play Button"
+          style={{
+            width: "20vw",
+            maxWidth: "150px",
+            
+            height: "auto",
+            cursor: "pointer",
+            transition: "0.3s",
+          }}
+          onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.8")}
+          onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
+          onClick={() => router.push("/playstate1")}
+        />
+       <img
+          src="/image/selectmons.png"
+          alt="Play Button"
+          style={{
+            width: "20vw",
+            maxWidth: "150px",
+            
+            height: "auto",
+            cursor: "pointer",
+            transition: "0.3s",
+          }}
+          onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.8")}
+          onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
+          onClick={() => router.push("/playstate1")}
+        />
+       <img
+          src="/image/selectmons.png"
+          alt="Play Button"
+          style={{
+            width: "20vw",
+            maxWidth: "150px",
+            
+            height: "auto",
+            cursor: "pointer",
+            transition: "0.3s",
+          }}
+          onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.8")}
+          onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
+          onClick={() => router.push("/playstate1")}
+        />
+    </Flex>
+    </div>
+  )
+}
---
+++ react-kombat/src/app/selectplayer/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+
+
+export default function selectplayer() {
+    const router = useRouter();
+  return (
+    <div style={{ 
+        width: "100vw",
+        height: "100vh",
+        position: "relative",
+        backgroundImage: "url(/image/Destop_player_select.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat"
+      }}>
+
+ {/* ปุ่ม Return */}
+<img
+        src="/image/return_button.png"
+        alt="Return Button"
+        style={{
+          position: "absolute",
+          top: "85%", // ปรับให้อยู่ด้านบนสุด
+          left: "5%", // ปรับให้อยู่ด้านซ้าย
+          width: "70px", // ปรับขนาดให้เล็กลง
+          height: "70px",
+          cursor: "pointer",
+          transition: "opacity 0.3s",
+          zIndex: 10, // ทำให้แสดงอยู่ด้านบน
+        }}
+        onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.8")}
+        onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
+        onClick={() => router.push("/Mode")}
+      />
+       {/* ปุ่ม home */}
+<img
+        src="/image/button_home.png"
+        alt="home Button"
+        style={{
+          position: "absolute",
+          top: "85%", // ปรับให้อยู่ด้านบนสุด
+          left: "14%", // ปรับให้อยู่ด้านซ้าย
+          width: "70px", // ปรับขนาดให้เล็กลง
+          height: "70px",
+          cursor: "pointer",
+          transition: "opacity 0.3s",
+          zIndex: 10, // ทำให้แสดงอยู่ด้านบน
+        }}
+        onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.8")}
+        onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
+        onClick={() => router.push("/Home")}
+      />
+
+
+<div>
+<img 
+      src="/image/player1.png" 
+      className="rounded img-fluid" 
+      alt="mode1" 
+      style={{ maxWidth: "200px",
+        position: "absolute",
+        top: "70%", // ปรับให้อยู่ด้านบนสุด
+        left: "20%",
+         cursor: "pointer" }} 
+      onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.9")}
+      onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
+      onClick={() => router.push("/player1")}  
+    />
+
+    <img 
+      src="/image/player2.png" 
+      className="rounded img-fluid" 
+      alt="mode3" 
+      style={{ maxWidth: "200px",
+        position: "absolute",
+        top: "70%", // ปรับให้อยู่ด้านบนสุด
+        left: "60%", 
+         cursor: "pointer"
+
+       }} 
+      onMouseEnter={(e) => (e.currentTarget.style.opacity = "0.9")}
+      onMouseLeave={(e) => (e.currentTarget.style.opacity = "1")}
+      onClick={() => router.push("/player2")}  
+    />
+
+   
+  
+</div>
+
+      </div>
+
+      
+  )
+}
